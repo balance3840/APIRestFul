e09b2052ad29f3b5509fbf71afe033c49aaf17a7
Community Manager <communitymanager@intras.com.do>
2018-02-02 10:23:04 -0400

    Commit inicial
    Inicio del proeycto

---
+++ app/Traits/ApiResponser.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Traits;
+
+Use Illuminate\Support\Collection;
+Use Illuminate\Database\Eloquent\Model;
+
+trait ApiResponser
+{
+	private function successReponse($data, $code)
+	{
+       return response()->json($data, $code);
+	}
+
+	protected function errorResponse($message, $code, $headers = ['Content-Type' => 'application/json; charset=UTF-8',
+            'charset' => 'utf-8'], $options = JSON_UNESCAPED_UNICODE)
+	{
+		return response()->json(['error' => $message, 'code' => $code], $code, $headers, $options);
+	}
+
+	protected function showAll(Collection $collection, $code = 200)
+	{
+       return $this->successReponse(['data' => $collection], $code);
+	}
+
+	protected function showOne(Model $instance, $code = 200)
+	{
+       return $this->successReponse(['data' => $instance], $code);
+	}
+}
