d09739cede10155ac9055f83669f5280ce0558b5
Ramiro <ramiro.estrellac@gmail.com>
2018-02-03 16:00:34 -0400

    ProductCategoryController

--- routes/api.php
+++ routes/api.php
@@ -28,6 +28,7 @@ Route::resource('categories.buyers','Category\CategorybuyerController',['only' =
 Route::resource('products','Product\ProductController',['only' => ['index','show']]);
 Route::resource('products.transactions','Product\ProductTransactionController',['only' => ['index']]);
 Route::resource('products.buyers','Product\ProductBuyerController',['only' => ['index']]);
+Route::resource('products.categories','Product\ProductCategoryController',['only' => ['index']]);
 
 Route::resource('transactions','Transaction\TransactionController',['only' => ['index','show']]);
 Route::resource('transactions.categories','Transaction\TransactionCategoryController',['only' => ['index']]);
