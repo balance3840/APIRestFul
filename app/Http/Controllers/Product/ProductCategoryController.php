db4d9289209bb55351a99fd42c33f7b9ae5ad125
Ramiro <ramiro.estrellac@gmail.com>
2018-02-04 00:29:13 -0400

    Finalizando ProductCategoryController

--- app/Http/Controllers/Product/ProductCategoryController.php
+++ app/Http/Controllers/Product/ProductCategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Product;
 
 use App\Product;
+use App\Category;
 use Illuminate\Http\Request;
 use App\Http\Controllers\ApiController;
 
@@ -18,6 +19,22 @@ class ProductCategoryController extends ApiController
         $categories = $product->categories;
 
         return $this->showAll($categories);
+    }
+
+    public function update(Request $request, Product $product, Category $category){
+        $product->categories()->syncWithoutDetaching($category->id);
+
+        return $this->showall($product->categories);
+    }
+
+    public function destroy(Product $product, Category $category){
+        if (!$product->categories()->find($category->id)) {
+            return $this->errorResponse('La categoria especificada no pertenece al producto', 404);
+        }
+
+        $product->categories()->detach($category->id);
+
+        return $this->showAll($product->categories);
     }
 
 }
